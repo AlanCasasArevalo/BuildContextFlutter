89af56fba3322d2f281dfc3444456730c8c32b06
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 11:23:51 +0100

    accediendo al contexto de la app principal

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Esta funcion findAncestorWidgetOfExactType nos devuelve el padre que encuentre (siempre del tipo generico que le especifiquemos)
+    final materialAppContext = context.findAncestorWidgetOfExactType<MaterialApp>();
+    print('El contexto de material app $materialAppContext');
     return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Text(
-            // No podemos acceder a un parametro como context si el widget aun no se ha renderizado
-            '${context.size.toString()}',
-          ),
-        ));
+      appBar: AppBar(),
+    );
   }
 }
