2849122812ec2321f7ba353086b96999635e1b72
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 12:38:47 +0100

    Este metodo va a provocar un crash en la aplicacion

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:build_context/pages/home_page.dart';
 import 'package:build_context/utils/screen_utils.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,20 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    // Lo que se haria en una aplicacion real llamada a servicios
+    // Una vez terminado todos los servicios navegamos a home
+    final route = MaterialPageRoute(builder: (_) => HomePage());
+    Navigator.pushReplacement(context, route);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
