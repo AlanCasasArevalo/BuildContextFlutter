a7a1e2683fb4db785af4b50ced7fb825a1fba904
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 12:08:53 +0100

    Se cambia la funcion para poder usar otro metodo

--- lib/utils/screen_utils.dart
+++ lib/utils/screen_utils.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 // Se crea una extension de BuildContext
 extension ScreenUtilsExtension on BuildContext {
-  Size get screenSize {
+
+  MediaQueryData get mediaQueryData {
     final mediaQuery = findAncestorWidgetOfExactType<MediaQuery>();
-    final screenSize = mediaQuery?.data.size;
-    if (screenSize != null) {
-      return screenSize;
-    } else {
-      return const Size(1, 1);
-    }
+    final mediaQueryData = mediaQuery!.data;
+    return mediaQueryData;
+  }
+
+  Size get screenSize {
+    final screenSize = mediaQueryData.size;
+    return screenSize;
   }
 }
