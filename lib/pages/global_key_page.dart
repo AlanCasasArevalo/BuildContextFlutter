e65e595c629f7991c2f2adc7d610d088f60d9f01
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-09 18:52:33 +0100

    GLOBALKEY

--- lib/pages/global_key_page.dart
+++ lib/pages/global_key_page.dart
@@ -57,8 +57,13 @@ class GlobalKeyPage extends StatelessWidget {
 
   void _printTextSize() {
     // Se imprime el contexto del Text en el que esta
-    print(_textKey.currentContext);
+    // print(_textKey.currentContext);
     // Se imprime el estado del Text en el que esta, OJO SI ES UN STATELESSWIDGET SERA NUL
-    print(_textKey.currentState);
+    // print(_textKey.currentState);
+
+    // OJO esto dara un crash si el _textKey no esta asignado
+    final renderBox = _textKey.currentContext!.findRenderObject() as RenderBox;
+    // Nos permite saber las dimensiones del texto
+    print(renderBox.size);
   }
 }
