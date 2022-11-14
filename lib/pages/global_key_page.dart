0f5465ffb64ddb5b32d94516aa758fd225e78c20
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-14 06:37:44 +0100

    GLOBALKEY

--- lib/pages/global_key_page.dart
+++ lib/pages/global_key_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/screen_utils.dart';
 
 class GlobalKeyPage extends StatelessWidget {
   final _textKey = GlobalKey();
@@ -50,12 +51,14 @@ class GlobalKeyPage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _printTextSize,
+        onPressed: () {
+          _printTextSize(context);
+        },
       ),
     );
   }
 
-  void _printTextSize() {
+  void _printTextSize(BuildContext context) {
     // Se imprime el contexto del Text en el que esta
     // print(_textKey.currentContext);
     // Se imprime el estado del Text en el que esta, OJO SI ES UN STATELESSWIDGET SERA NUL
@@ -64,10 +67,16 @@ class GlobalKeyPage extends StatelessWidget {
     // OJO esto dara un crash si el _textKey no esta asignado o bien el widget se ha renderizado completamente
     final renderBox = _textKey.currentContext!.findRenderObject() as RenderBox;
 
+    final screenSize = context.screenSize;
+
     // con estas instrucciones lo que conseguimos es la localizacion exacta del widget respecto a su posicion en toda la pantalla
     final position = renderBox.localToGlobal(
-      // Punto de referencia inicio de pantalla superior izquierda
-      Offset(0, 0),
+
+      // Se obtiene la posicion del widget respecto al centro de la pantalla.
+      Offset(
+        -screenSize.width*0.5,
+        -screenSize.height*.5,
+      ),
     );
     print(position);
 
